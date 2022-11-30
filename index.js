2de442b14e75b9277190372b3b10d00411b66e9a
dj3rry <davis.jerryjr@gmail.com>
2022-11-30 18:52:19 -0500

    updated index.js code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./generateMarkdown");
-const 
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,11 +15,6 @@ const questions = [
     {
         name: "description",
         message: "Enter a description of your project.",
-        type: "input",
-    },
-    {
-        name: "TOC",
-        message: "Enter your table of contents.",
         type: "input",
     },
     {
@@ -39,18 +34,19 @@ const questions = [
     },
     {
         name: "license",
-        message: "List all collaborators and/or third-party assets.",
-        type: "input",
+        message: "List license used for this project.",
+        type: "list",
+        choices: ["MIT"," "],
     },
 
 ];
 
 // TODO: Create a function to write README file
 
-function writeToFile(fileName, data) {
-    fs.writeToFile(fileName, data, error => {
-        if(error) {
-            return console.log(error);
+function writeToFile(data) {
+    fs.writeFile('ExampleREADME.md', data, err => {
+        if(err) {
+            return console.error(err);
         }
         else {
             console.log("Your readme file has been created.")
@@ -60,8 +56,17 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    return inquirer.prompt(quesitons);
+    return inquirer.prompt(questions);
 }
 
 // Function call to initialize app
-init();
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(error => {
+    console.log(error);
+});
